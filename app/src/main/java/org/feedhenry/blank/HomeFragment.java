150bf20832fe35dfaed234842c485a15cbbeb180
Daniel Passos <daniel@passos.me>
2017-11-01 13:46:06 -0200

    Solve some lint errors

--- app/src/main/java/org/feedhenry/blank/HomeFragment.java
+++ app/src/main/java/org/feedhenry/blank/HomeFragment.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Red Hat, Inc., and individual contributors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,29 +15,27 @@
  */
 package org.feedhenry.blank;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.os.*;
+import android.support.annotation.*;
+import android.support.v4.app.*;
+import android.view.*;
+import android.widget.*;
 
 public class HomeFragment extends Fragment {
 
-	public static final String MESSAGE = "MESSAGE";
+    public static final String MESSAGE = "MESSAGE";
 
-	@Nullable
-	@Override
-	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-							 @Nullable Bundle savedInstanceState) {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.home_fragment, null);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
 
-		TextView textView = (TextView) view.findViewById(R.id.result);
-		textView.setText(getArguments().getString(MESSAGE));
+        TextView textView = view.findViewById(R.id.result);
+        textView.setText(getArguments().getString(MESSAGE));
 
-		return view;
-	}
+        return view;
+    }
 
 }
